ee2317e580afb6eb07ef1dff603ed4bdfb5199ca
whiteroma <roma.white95@gmail.com>
2022-08-01 17:37:09 +0300

    .

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,13 @@
+// Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +22,18 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector('.gallery');
+
+const imgs = images
+.map(image => `<li><img class="picture" src=${image.url} width = '400' alt='${image.alt}'></img></li>`)
+.join('')
+list.insertAdjacentHTML('beforeend', imgs);
+
+list.style.display = 'flex';
+list.style.flexDirection = 'column'
+list.style.listStyle = 'none'
+list.style.justifyContent = 'center'
+list.style.alignItems = 'center'
+
+
---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+let counterValue = 0
+
+const values = document.querySelector('#value')
+const buttonIncr = document.querySelector('button[data-action="increment"]')
+const buttonDecr = document.querySelector('button[data-action="decrement"]')
+buttonIncr.addEventListener('click', () => {
+    counterValue++
+	values.textContent = counterValue
+})
+
+buttonDecr.addEventListener('click', () => {
+    counterValue--
+	values.textContent = counterValue
+})
