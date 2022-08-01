6ca42e2476ab7b59648e6940d6970fb590dff3fc
whiteroma <roma.white95@gmail.com>
2022-08-01 16:06:23 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const El = document.querySelector("ul").children.length;
+console.log("Number of categories:", El);
+
+const ul = document.querySelectorAll('#categories>li');
+ul.forEach(el => {
+
+  console.log(
+    `Категория: ${el.firstElementChild.textContent}, Количество элементов: ${el.lastElementChild.children.length}`,
+  );
+});
